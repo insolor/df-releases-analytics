c61e9deef1d07da3b2b7fe9390799951922dca80
insolor <2442833+insolor@users.noreply.github.com>
2024-12-14 14:22:24 +0300

    Group by months

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title="Dwarf Fortress releases per quarter", layout="wide")
+title = "Dwarf Fortress releases analytics"
+st.set_page_config(page_title=title, layout="wide", page_icon="📈")
 
-st.header("Dwarf Fortress releases per quarter")
+st.header(title)
 
 df = pd.read_csv("releases.csv")
 
@@ -17,50 +18,50 @@ df.sort_values(by=[date_column], inplace=True, ignore_index=True)
 
 df[date_column] = pd.to_datetime(df[date_column])
 
-start_date = df[date_column].min().to_period("Q")
-end_date = df[date_column].max().to_period("Q")
-all_months = pd.period_range(start=start_date, end=end_date, freq="Q")
+start_date = df[date_column].min().to_period("M")
+end_date = df[date_column].max().to_period("M")
+all_months = pd.period_range(start=start_date, end=end_date, freq="M")
 
-df["quarter"] = df[date_column].dt.to_period("Q")
+df["bucket"] = df[date_column].dt.to_period("M")
 df["version_with_date"] = df.apply(
     lambda row: (row["version_number"], row["release_date"].date().isoformat()), axis=1
 )
 
-quarterly_counts = (
-    df.groupby("quarter")
+buckets = (
+    df.groupby("bucket")
     .agg({"version_with_date": lambda x: list(x)})
     .reindex(all_months, fill_value=[])
 )
 
-quarterly_counts.index = quarterly_counts.index.astype(str)
-quarterly_counts["count"] = quarterly_counts["version_with_date"].apply(len)
+buckets.index = buckets.index.astype(str)
+buckets["count"] = buckets["version_with_date"].apply(len)
 
 
 def format_version_with_date(versions_with_dates: list[tuple[str, str]]) -> str:
     return ", ".join([f"{version} ({day})" for version, day in versions_with_dates])
 
 
-quarterly_counts["versions"] = quarterly_counts["version_with_date"].apply(
+buckets["versions"] = buckets["version_with_date"].apply(
     format_version_with_date
 )
 
 interval = alt.selection_interval(encodings=["x"])
 
-chart_data = quarterly_counts.reset_index().rename(columns={"index": "quarter"})
+chart_data = buckets.reset_index().rename(columns={"index": "bucket"})
 chart = (
     alt.Chart(chart_data)
     .mark_bar()
     .encode(
-        x=alt.X("quarter:O", title="Quarter"),
+        x=alt.X("bucket:O", title="Month"),
         y=alt.Y("count:Q", title="Count"),
         tooltip=[
-            alt.Tooltip("quarter:O", title="Quarter"),
+            alt.Tooltip("bucket:O", title="Month"),
             alt.Tooltip("count:Q", title="Count"),
             alt.Tooltip("versions:N", title="Releases"),
         ],
     )
     .properties(
-        title="Count of releases per quarter",
+        title="Count of releases",
         height=300,
     )
     .add_params(interval)
@@ -70,9 +71,9 @@ event = st.altair_chart(chart, use_container_width=True, on_select="rerun")
 
 selection = event["selection"]["param_1"]
 if selection:
-    quarters = selection["quarter"]
+    bucket = selection["bucket"]
     versions_with_dates = (
-        quarterly_counts.loc[quarters[0] : quarters[-1]]["version_with_date"]
+        buckets.loc[bucket[0] : bucket[-1]]["version_with_date"]
         .apply(pd.Series)
         .stack()
         .unique()
