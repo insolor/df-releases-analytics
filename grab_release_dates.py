bbfe313570a78efdc6f232d4356ac5bf9c8b4a52
insolor <2442833+insolor@users.noreply.github.com>
2025-02-13 00:28:52 +0300

    Remove unused exception variable

--- grab_release_dates.py
+++ grab_release_dates.py
@@ -55,7 +55,7 @@ def get_release_info() -> pd.DataFrame:
         
         try:
             result.append(parse_version(version_text))
-        except ValueError as e:
+        except ValueError:
             print(f"Could not parse version text: {version_text!r}")
 
     return pd.DataFrame(reversed(result))
